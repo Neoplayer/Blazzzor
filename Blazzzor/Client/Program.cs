04c4225e4d5caa35d0598e1c6f4162ddc6b476a4
Павел Николаевич Нестерчук <pas25151@niuitmo.ru>
2019-12-08 19:53:53 +0300

    Добавьте файлы проекта.

---
+++ Blazzzor/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace Blazzzor.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
---
+++ Blazzzor/Server/Controllers/PetController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blazzzor.Server.Controllers
+{
+    #region snippet_Inherit
+    [ApiController]
+    [Produces(MediaTypeNames.Application.Json)]
+    //[Route("[controller]")]
+    public class PetsController : ControllerBase
+    #endregion
+    {
+        private static readonly List<Pet> _petsInMemoryStore = new List<Pet>();
+
+        public PetsController()
+        {
+            if (_petsInMemoryStore.Count == 0)
+            {
+                _petsInMemoryStore.Add(
+                    new Pet
+                    {
+                        Breed = "Collie",
+                        Id = 1,
+                        Name = "Fido"
+                    });
+            }
+        }
+
+        [HttpGet]
+        [Route("[Controller]/GetAll")]
+        public ActionResult<List<Pet>> GetAll() => _petsInMemoryStore;
+
+        [HttpGet("{id}")]
+
+        [Route("[Controller]/GetById/{id:int}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<Pet> GetById(int id)
+        {
+            var pet = _petsInMemoryStore.FirstOrDefault(p => p.Id == id);
+
+            #region snippet_ProblemDetailsStatusCode
+            if (pet == null)
+            {
+                return NotFound();
+            }
+            #endregion
+
+            return pet;
+        }
+
+        #region snippet_400And201
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [Route("[Controller]/Create")]
+        public ActionResult<Pet> Create(Pet pet)
+        {
+            pet.Id = _petsInMemoryStore.Any() ?
+                _petsInMemoryStore.Max(p => p.Id) + 1 : 1;
+            _petsInMemoryStore.Add(pet);
+
+            return CreatedAtAction(nameof(GetById), new { id = pet.Id }, pet);
+        }
+        #endregion
+    }
+
+    public class Pet
+    {
+        public Int32 Id { get; set; }
+        public string Name { get; set; }
+        public String Breed { get; set; }
+    }
+}
